5a2065f5be195ecb89f75abea46eca13a1fe17db
Aleksey Loktionov <durf19851@gmail.com>
2022-10-19 07:25:23 +0700

    DO-60 Create stage and prod evironments Move config files (main.tf,outputs.tf,variables.tf) into 2 directory (prod,stage), added into these directory file terraform.tfvars.example IP address in VPC module not changed because will be push to github my own IP

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -13,20 +13,20 @@ provider "google" {
 }
 
 module "app" {
-  source          = "./modules/app"
+  source          = "../modules/app"
   public_key_path = var.public_key_path
   zone            = var.zone
   app_disk_image  = var.app_disk_image
 }
 module "db" {
-  source          = "./modules/db"
+  source          = "../modules/db"
   public_key_path = var.public_key_path
   zone            = var.zone
   db_disk_image   = var.db_disk_image
 }
 module "vpc" {
-  source        = "./modules/vpc"
-  source_ranges = ["0.0.0.0/0"] 
+  source        = "../modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
 
 }
 resource "google_compute_project_metadata" "my_ssh_key" {
@@ -34,5 +34,3 @@ resource "google_compute_project_metadata" "my_ssh_key" {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
-
-
