656af94ed7939d636923a5f5bc2a38c9580731a1
Aleksey Loktionov <durf19851@gmail.com>
2022-12-11 08:36:56 +0700

    DO-83 Create stage and prod environments
    The directory structure has been changed. Files were sorted according
    to the directory structure. Created 2 environments stage and prod

--- ansible_deploy.sh
+++ ansible_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
-packer build -var-file="$SCRIPT_PATH"/packer/two_vm_ansible_provisioner/variables.json  "$SCRIPT_PATH"/packer/two_vm_ansible_provisioner/app.json
-packer build -var-file="$SCRIPT_PATH"/packer/two_vm_ansible_provisioner/variables.json  "$SCRIPT_PATH"/packer/two_vm_ansible_provisioner/db.json
+packer build -var-file="$SCRIPT_PATH"/packer/variables.json  "$SCRIPT_PATH"/packer/app.json
+packer build -var-file="$SCRIPT_PATH"/packer/variables.json  "$SCRIPT_PATH"/packer/db.json
